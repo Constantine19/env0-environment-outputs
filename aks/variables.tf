9d3a6ab3961531c3c36d09ab672db500c9326bc9
Konstantin Zelmanovich <Constantine19>
2024-06-02 23:09:27 +0300

    test

--- aks/variables.tf
+++ aks/variables.tf
@@ -3,3 +3,18 @@ variable "vnet_subnet_id" {
   type        = string
   description = "ID of the subnet to deploy the AKS cluster into."
 }
+
+variable "length" {
+  type    = number
+  default = 15
+}
+
+variable "refresh_token" {
+  type    = string
+  default = "0"
+}
+
+variable "numeric" {
+    type  = bool
+    default = false
+}
